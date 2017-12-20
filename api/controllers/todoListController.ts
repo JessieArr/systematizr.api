5e8faa901809f078220b9680b28629503400ddc0
Jessie Arr <smorrison1986@gmail.com>
2017-12-19 23:00:38 -0600

    Finally got the API working!
    Hit /tasks in a browser to see it return a default object!

---
+++ api/controllers/todoListController.ts
@@ -0,0 +1,26 @@
+
+exports.list_all_tasks = function(req : any, res : any) {
+    res.json({"test": "test"});
+};
+
+
+
+
+exports.create_a_task = function(req : any, res : any) {
+    res.json({"test": "test"});
+};
+
+
+exports.read_a_task = function(req : any, res : any) {
+    res.json({"test": "test"});
+};
+
+
+exports.update_a_task = function(req : any, res : any) {
+    res.json({"test": "test"});
+};
+
+
+exports.delete_a_task = function(req : any, res : any) {
+    res.json({ message: 'Task successfully deleted' });
+};
